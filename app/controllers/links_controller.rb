fee7bf0ccd42eb30138d8ac74fa19eadd9cc3fee
Yosef Benny Widyokarsono <yosebennywidyo@gmail.com>
2023-01-02 00:46:44 +0800

    Update LinksController: - remove unused method - initialize `@link`

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,10 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: %i[ show edit update destroy ]
+  before_action :set_link, only: %i[ show ]
 
   # GET /links or /links.json
   def index
-    @links = Link.all
+    @links  = Link.all
+    @link   = Link.new
   end
 
   # GET /links/1 or /links/1.json
@@ -18,45 +19,18 @@ class LinksController < ApplicationController
     @link = Link.new
   end
 
-  # GET /links/1/edit
-  def edit
-  end
-
   # POST /links or /links.json
   def create
     @link = Link.new(link_params)
 
     respond_to do |format|
       if @link.save
-        format.html { redirect_to link_url(@link), notice: "Link was successfully created." }
-        format.json { render :show, status: :created, location: @link }
+        format.html { redirect_to links_url, notice: "Link was successfully created." }
+        format.json { render :index, status: :created }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { redirect_to links_url, notice: @link.errors.full_messages}
         format.json { render json: @link.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /links/1 or /links/1.json
-  def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to link_url(@link), notice: "Link was successfully updated." }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /links/1 or /links/1.json
-  def destroy
-    @link.destroy
-
-    respond_to do |format|
-      format.html { redirect_to links_url, notice: "Link was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 

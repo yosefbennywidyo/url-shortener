a33a27b86a741a44de048c73c542c2a1644b419a
Yosef Benny Widyokarsono <yosebennywidyo@gmail.com>
2023-01-02 00:59:48 +0800

    Update routes: - add index and create links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/s/:slug', to: 'links#show', as: :short
+  resources :links, only: [:index, :create]
   root 'links#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
